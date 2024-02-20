6464a7520492938d4fef62ce35472641b84416b9
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 04:54:00 -0700

    Implementación
    Implementación de la interfaz ITransferenciaDAO

--- PersistenciaBanco/src/main/java/dao/ITransferenciaDAO.java
+++ PersistenciaBanco/src/main/java/dao/ITransferenciaDAO.java
@@ -4,10 +4,33 @@
  */
 package dao;
 
+import com.mycompany.dominiobanco.Transferencia;
+
+import dto.TransferenciaNuevaDTO;
+import excepciones.PersistenciaException;
+
 /**
- *
+ * Esta interfaz posee los métodos para acceder y manipular los datos de la
+ * tabla Transferencia en la base de datos.
+ * 
  * @author ID145
  */
 public interface ITransferenciaDAO {
-    
+    /**
+     * Este método se encarga de agregar una nueva transferencia a la base de datos.
+     * 
+     * @param transferenciaNueva DTO con los datos de la transferencia a agregar
+     * @return Transferencia agregada
+     * @throws PersistenciaException
+     */
+    Transferencia agregar(TransferenciaNuevaDTO transferenciaNueva) throws PersistenciaException;
+
+    /**
+     * Este método se encarga de consultar una transferencia por su ID.
+     * 
+     * @param id_transferencia
+     * @return Transferencia consultada
+     * @throws PersistenciaException
+     */
+    Transferencia consultarTransferencia(int cuenta) throws PersistenciaException;
 }
