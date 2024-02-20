775b23499742a674a3ad33c81bf21576def4ffbc
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 03:34:59 -0700

    Métodos
    creación de métodos equals y hashcode.

--- DominioBanco/src/main/java/com/mycompany/dominiobanco/Transferencia.java
+++ DominioBanco/src/main/java/com/mycompany/dominiobanco/Transferencia.java
@@ -10,4 +10,94 @@ package com.mycompany.dominiobanco;
  */
 public class Transferencia {
     
+    private int id_transaccion;
+    private int cuenta_destino;
+
+    /**
+     * Constructor de la clase Transferencia por defecto.
+     */
+    public Transferencia() {
+    }
+
+    /**
+     * Constructor de la clase Transferencia que inicializa todos los campos.
+     * 
+     * @param id_transaccion El ID de la transacción.
+     * @param cuenta_destino El número de cuenta destino.
+     */
+    public Transferencia(int id_transaccion, int cuenta_destino) {
+        this.id_transaccion = id_transaccion;
+        this.cuenta_destino = cuenta_destino;
+    }
+
+    /**
+     * Método que devuelve el ID de la transacción.
+     * 
+     * @return El ID de la transacción.
+     */
+    public int getId_transaccion() {
+        return id_transaccion;
+    }
+
+    /**
+     * Método que devuelve el número de cuenta destino.
+     * 
+     * @return El número de cuenta destino.
+     */
+    public int getCuenta_destino() {
+        return cuenta_destino;
+    }
+
+    /**
+     * Método que establece el ID de la transacción
+     * 
+     * @param id El ID de la transacción.
+     */
+    public void setId_transaccion(int id) {
+        this.id_transaccion = id;
+    }
+
+    /**
+     * Método que establece el número de cuenta destino.
+     * 
+     * @param cuenta El número de cuenta destino.
+     */
+    public void setCuenta_destino(int cuenta) {
+        this.cuenta_destino = cuenta;
+    }
+
+    /**
+     * Devuelve un valor hash code para el objeto.
+     * 
+     * @return Un valor hash code para el objeto.
+     */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.id_transaccion;
+        return hash;
+
+    }
+
+    /**
+     * Método que compara dos objetos de tipo Transferencia.
+     * 
+     * @param obj El objeto a comparar.
+     * @return true si los objetos son iguales, false en otro caso.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Transferencia other = (Transferencia) obj;
+        return this.id_transaccion == other.id_transaccion;
+    }
+
 }
