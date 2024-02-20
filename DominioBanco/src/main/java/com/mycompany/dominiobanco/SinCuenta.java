06e7cc4989cc401db02824f772730c8d54816638
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 03:16:30 -0700

    Setters/Getters
    Métodos de acceso para la clase SinCuenta.

--- DominioBanco/src/main/java/com/mycompany/dominiobanco/SinCuenta.java
+++ DominioBanco/src/main/java/com/mycompany/dominiobanco/SinCuenta.java
@@ -45,5 +45,69 @@ public class SinCuenta {
         this.folio = folio;
         this.contrasena = contrasena;
     }
+
+    /**
+     * Método que devuelve el ID de la transacción.	
+     * @return El ID de la transacción.
+     */
+    public int getId_transaccion() {
+        return id_transaccion;
+    }
+
+    /**
+     * Método que establece el ID de la transacción
+     * @param id El ID de la transacción.
+     */
+    public void setId_transaccion(int id) {
+        this.id_transaccion = id;
+    }
+
+    /**
+     * Método que devuelve el estado de la transacción.
+     * @return El estado de la transacción.
+     */
+    public String getEstado() {
+        return estado;
+    }
     
+    /**
+     * Método que establece el estado de la transacción.
+     * @param estado El estado de la transacción.
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * Método que devuelve el folio de la transacción.
+     * @return El folio de la transacción.
+     */
+    public int getFolio() {
+        return folio;
+    }
+
+    /**
+     * Método que establece el folio de la transacción.
+     * @param folio El folio de la transacción.
+     */
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    /**
+     * Método que devuelve la contraseña de la transacción.
+     * @return La contraseña de la transacción.
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * Método que establece la contraseña de la transacción.
+     * @param contrasena La contraseña de la transacción.
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
 }
