38df28ef353062af2edae2725a560421d10aa6c0
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 03:13:46 -0700

    Creacion
    Creación de constructores de la clase SinCuenta.

--- DominioBanco/src/main/java/com/mycompany/dominiobanco/SinCuenta.java
+++ DominioBanco/src/main/java/com/mycompany/dominiobanco/SinCuenta.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.dominiobanco;
 
 /**
+ * Clase que representa una cuenta bancaria.
  *
  * @author ID145
  */
 public class SinCuenta {
+    private int id_transaccion;
+    private String estado;
+    private int folio;
+    private String contrasena;
+
+    /**
+     * Constructor de la clase SinCuenta por defecto.
+     */
+    public SinCuenta() {
+    }
+
+    /**
+     * Constructor de la clase SinCuenta que inicializa todos los campos.
+     *
+     * @param id_transaccion El ID de la transacción.
+     * @param estado
+     * @param folio
+     * @param contrasena
+     */
+    public SinCuenta(int id_transaccion, String estado, int folio, String contrasena) {
+        this.id_transaccion = id_transaccion;
+        this.estado = estado;
+        this.folio = folio;
+        this.contrasena = contrasena;
+    }
+
+    /**
+     * Constructor de la clase SinCuenta que inicializa todos los campos excepto el
+     * ID de la transacción.
+     *
+     * @param estado
+     * @param folio
+     * @param contrasena
+     */
+    public SinCuenta(String estado, int folio, String contrasena) {
+        this.estado = estado;
+        this.folio = folio;
+        this.contrasena = contrasena;
+    }
     
 }
