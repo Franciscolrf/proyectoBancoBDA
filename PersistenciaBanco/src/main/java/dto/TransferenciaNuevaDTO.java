ab8b4fa7cff831a79da030c58482439146922ac6
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 05:01:21 -0700

    implementación
    implementación de la clase TransferenciaNuevaDTO

--- PersistenciaBanco/src/main/java/dto/TransferenciaNuevaDTO.java
+++ PersistenciaBanco/src/main/java/dto/TransferenciaNuevaDTO.java
@@ -5,9 +5,54 @@
 package dto;
 
 /**
- *
+ * Esta clase representa un DTO para la transferencia de dinero entre cuentas.
+ * 
  * @author ID145
  */
 public class TransferenciaNuevaDTO {
-    
+    private int id_transaccion;
+    private int cuenta_destino;
+
+    /**
+     * Constructor por defecto de la clase TransferenciaNuevaDTO.
+     */
+    public TransferenciaNuevaDTO() {
+    }
+
+    /**
+     * Obtiene el ID de la transacción.
+     *
+     * @return El ID de la transacción.
+     */
+    public int getId_transaccion() {
+        return id_transaccion;
+    }
+
+    /**
+     * Establece el ID de la transacción.
+     *
+     * @param id_transaccion El ID de la transacción a establecer.
+     */
+    public void setId_transaccion(int id_transaccion) {
+        this.id_transaccion = id_transaccion;
+    }
+
+    /**
+     * Obtiene el número de cuenta de destino.
+     *
+     * @return El número de cuenta de destino.
+     */
+    public int getCuenta_destino() {
+        return cuenta_destino;
+    }
+
+    /**
+     * Establece el número de cuenta de destino.
+     *
+     * @param cuenta_destino El número de cuenta de destino a establecer.
+     */
+    public void setCuenta_destino(int cuenta_destino) {
+        this.cuenta_destino = cuenta_destino;
+    }
+
 }
