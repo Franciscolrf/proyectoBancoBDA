648be8b78e366ed4af60e7ed1d9e3092c72a4002
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 04:11:23 -0700

    Creacion
    Creación del resto de métodos para la interfaz IClienteDAO

--- PersistenciaBanco/src/main/java/dao/IClienteDAO.java
+++ PersistenciaBanco/src/main/java/dao/IClienteDAO.java
@@ -1,6 +1,8 @@
 
 package dao;
 
+import java.util.List;
+
 import com.mycompany.dominiobanco.Cliente;
 import dto.ClienteNuevoDTO;
 import excepciones.PersistenciaException;
@@ -20,5 +22,44 @@ public interface IClienteDAO {
      */
     Cliente agregar (ClienteNuevoDTO clienteNuevo) throws PersistenciaException;
 
-    
+    /**
+     * Consulta todos los clientes de la base de datos.
+     * @return una lista con todos los clientes de la base de datos.
+     * @throws PersistenciaException
+     */
+    List<ClienteDAO> consultar() throws PersistenciaException;
+
+    /**
+     * Consulta un cliente por su identificador.
+     * @param id el identificador del cliente a consultar.
+     * @return el cliente consultado.
+     * @throws PersistenciaException si ocurre un error al intentar consultar el cliente.
+     */
+    Cliente consultarCliente(int usuario) throws PersistenciaException;
+
+    /**
+     * Consulta un cliente por su usuario y contraseña.
+     * @param usuario el usuario del cliente a consultar.
+     * @param contrasenia la contraseña del cliente a consultar.
+     * @return el cliente consultado.
+     * @throws PersistenciaException si ocurre un error al intentar consultar el cliente.
+     */
+    Cliente consultarCliente(String usuario, String contrasenia) throws PersistenciaException;
+
+    /**
+     * Actualiza un cliente en la base de datos.
+     * @param clienteNuevo los datos del cliente a actualizar.
+     * @param cliente el cliente a actualizar.
+     * @return el cliente actualizado.
+     * @throws PersistenciaException si ocurre un error al intentar actualizar el cliente.
+     */
+    Cliente actualizar(ClienteNuevoDTO clienteNuevo, Cliente cliente) throws PersistenciaException;
+
+    /**
+     * Elimina un cliente de la base de datos.
+     * @param id el identificador del cliente a eliminar.
+     * @return el cliente eliminado.
+     * @throws PersistenciaException si ocurre un error al intentar eliminar el cliente.
+     */
+    public Cliente eliminar(int id) throws PersistenciaException;
 }
