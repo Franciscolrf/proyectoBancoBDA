440f17bd92a1b9f297ae9f2c7526565664f9de59
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 04:56:03 -0700

    Métodos
    Creación de los métodos de la interfaz ITransaccionDAO

--- PersistenciaBanco/src/main/java/dao/ITransaccionDAO.java
+++ PersistenciaBanco/src/main/java/dao/ITransaccionDAO.java
@@ -4,10 +4,20 @@
  */
 package dao;
 
+import java.util.List;
+
+import com.mycompany.dominiobanco.Transaccion;
+
+import dto.TransaccionNuevaDTO;
+import excepciones.PersistenciaException;
+
 /**
- *
+ * Esta interfaz posee los métodos para acceder y manipular los datos de la tabla Transaccion en la base de datos.
  * @author ID145
  */
 public interface ITransaccionDAO {
-    
+    Transaccion agregar(TransaccionNuevaDTO transaccionNueva) throws PersistenciaException;
+    Transaccion consultarTransaccion(int id) throws PersistenciaException;
+    List<Transaccion> consultar(int id) throws PersistenciaException;
+    List<Transaccion> consultarPeriodo(int id, String desde, String hasta) throws PersistenciaException;
 }
