0b9fc345154bac31ae86894783f7071362c68d17
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 02:47:30 -0700

    Creación
    Creación de atributos de la entidad Cuenta.

--- DominioBanco/src/main/java/com/mycompany/dominiobanco/Cuenta.java
+++ DominioBanco/src/main/java/com/mycompany/dominiobanco/Cuenta.java
@@ -1,5 +1,13 @@
 package com.mycompany.dominiobanco;
 
+/**
+ * Clase que representa una cuenta bancaria.
+ */
 public class Cuenta {
-    
+    private int id_cuenta;
+    private String fecha_apertura;
+    private int numero;
+    private float saldo;
+    private int id_cliente;
+    private boolean esta_activo;
 }
