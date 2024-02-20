dd4d0583a056fd3a678f94d8841d049bda7e26a3
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 03:02:50 -0700

    Atributos
    Atributos de la clase Domicilio

--- DominioBanco/src/main/java/com/mycompany/dominiobanco/Domicilio.java
+++ DominioBanco/src/main/java/com/mycompany/dominiobanco/Domicilio.java
@@ -1,5 +1,21 @@
 package com.mycompany.dominiobanco;
 
+/**
+ * Clase que representa un domicilio.
+ */
 public class Domicilio {
-    
+    private int id_domicilio;
+    private String calle;
+    private int numero_exterior;
+    private int numero_interior;
+    private int codigo_postal;
+    private String colonia;
+    private int id_cliente;
+
+    /*
+     * Constructor de la clase Domicilio por defecto.
+     */
+    public Domicilio(){
+        
+    }
 }
