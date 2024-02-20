909fc79edecdea25b8d639e2266cbf8b422a55c9
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 04:57:18 -0700

    Documentacion
    Documentación de los métodos de la interfaz TransaccionDAO

--- PersistenciaBanco/src/main/java/dao/ITransaccionDAO.java
+++ PersistenciaBanco/src/main/java/dao/ITransaccionDAO.java
@@ -16,8 +16,37 @@ import excepciones.PersistenciaException;
  * @author ID145
  */
 public interface ITransaccionDAO {
+    /**
+     * Este método se encarga de agregar una nueva transacción a la base de datos.
+     * @param transaccionNueva
+     * @return
+     * @throws PersistenciaException en caso de que no se pueda agregar la transacción.
+     */
     Transaccion agregar(TransaccionNuevaDTO transaccionNueva) throws PersistenciaException;
+
+    /**
+     * Este método se encarga de consultar una transacción por su ID.
+     * @param id 
+     * @return Transacción consultada
+     * @throws PersistenciaException en caso de que no se encuentre la transacción. 
+     */
     Transaccion consultarTransaccion(int id) throws PersistenciaException;
+
+    /**
+     *  Este método se encarga de consultar todas las transacciones de una cuenta.
+     * @param id 
+     * @return Transacciones consultadas
+     * @throws PersistenciaException en caso de que no se encuentren transacciones para la cuenta.
+     */
     List<Transaccion> consultar(int id) throws PersistenciaException;
+
+    /**
+     * Este método se encarga de consultar todas las transacciones de una cuenta en un periodo de tiempo.
+     * @param id
+     * @param desde
+     * @param hasta
+     * @return Transacciones consultadas
+     * @throws PersistenciaException en caso de que no se encuentren transacciones para la cuenta.
+     */
     List<Transaccion> consultarPeriodo(int id, String desde, String hasta) throws PersistenciaException;
 }
