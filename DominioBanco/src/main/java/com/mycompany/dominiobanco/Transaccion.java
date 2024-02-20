fe8633eedf9edd9925abcfe56006e06822156745
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 03:26:34 -0700

    Implementacion
    Fin de la implementación de la clase Transaccion

--- DominioBanco/src/main/java/com/mycompany/dominiobanco/Transaccion.java
+++ DominioBanco/src/main/java/com/mycompany/dominiobanco/Transaccion.java
@@ -2,6 +2,7 @@ package com.mycompany.dominiobanco;
 
 /**
  * Clase que representa una transacción.
+ * 
  * @author ID145
  */
 public class Transaccion {
@@ -20,10 +21,10 @@ public class Transaccion {
     /**
      * Constructor de la clase Transaccion que inicializa todos los campos.
      * 
-     * @param id El ID de la transacción.
-     * @param monto El monto de la transacción.
-     * @param tipo El tipo de transacción.
-     * @param fecha La fecha de la transacción.
+     * @param id        El ID de la transacción.
+     * @param monto     El monto de la transacción.
+     * @param tipo      El tipo de transacción.
+     * @param fecha     La fecha de la transacción.
      * @param id_cuenta El ID de la cuenta asociada a la transacción.
      * 
      */
@@ -36,11 +37,12 @@ public class Transaccion {
     }
 
     /**
-     * Constructor de la clase Transaccion que inicializa todos los campos excepto el ID de la transacción.
+     * Constructor de la clase Transaccion que inicializa todos los campos excepto
+     * el ID de la transacción.
      * 
-     * @param monto El monto de la transacción.
-     * @param tipo El tipo de transacción.
-     * @param fecha La fecha de la transacción.
+     * @param monto     El monto de la transacción.
+     * @param tipo      El tipo de transacción.
+     * @param fecha     La fecha de la transacción.
      * @param id_cuenta El ID de la cuenta
      * 
      */
@@ -50,11 +52,13 @@ public class Transaccion {
         this.fecha = fecha;
         this.id_cuenta = id_cuenta;
     }
+
     /**
-     * Constructor de la clase Transaccion que inicializa todos los campos excepto el ID de la transacción y el ID de la cuenta.
+     * Constructor de la clase Transaccion que inicializa todos los campos excepto
+     * el ID de la transacción y el ID de la cuenta.
      * 
      * @param monto El monto de la transacción.
-     * @param tipo El tipo de transacción.
+     * @param tipo  El tipo de transacción.
      * @param fecha La fecha de la transacción.
      * 
      */
@@ -64,6 +68,124 @@ public class Transaccion {
         this.fecha = fecha;
     }
 
-    
+    /**
+     * Devuelve el ID de la transacción.
+     *
+     * @return El ID de la transacción.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Establece el ID de la transacción.
+     *
+     * @param id El nuevo ID de la transacción.
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Devuelve el monto de la transacción.
+     *
+     * @return El monto de la transacción.
+     */
+    public float getMonto() {
+        return monto;
+    }
+
+    /**
+     * Establece el monto de la transacción.
+     *
+     * @param monto El nuevo monto de la transacción.
+     */
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    /**
+     * Devuelve el tipo de la transacción.
+     *
+     * @return El tipo de la transacción.
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * Establece el tipo de la transacción.
+     *
+     * @param tipo El nuevo tipo de la transacción.
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * Devuelve la fecha de la transacción.
+     *
+     * @return La fecha de la transacción.
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * Establece la fecha de la transacción.
+     *
+     * @param fecha La nueva fecha de la transacción.
+     */
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * Devuelve el ID de la cuenta asociada a la transacción.
+     *
+     * @return El ID de la cuenta asociada a la transacción.
+     */
+    public int getId_cuenta() {
+        return id_cuenta;
+    }
+
+    /**
+     * Establece el ID de la cuenta asociada a la transacción.
+     *
+     * @param id_cuenta El nuevo ID de la cuenta asociada a la transacción.
+     */
+    public void setId_cuenta(int id_cuenta) {
+        this.id_cuenta = id_cuenta;
+    }
+
+    @Override
+    /**
+     * Devuelve un valor hash para el objeto.
+     * 
+     * @return Un valor hash para el objeto.
+     */
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    /**
+     * Método que compara dos objetos de tipo Transaccion.
+     * @param obj El objeto a comparar.
+     * @return true si los objetos son iguales, false en otro caso.
+     */
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this==obj) {
+            return true;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Transaccion other = (Transaccion) obj;
+        return this.id == other.id;
 
 }
