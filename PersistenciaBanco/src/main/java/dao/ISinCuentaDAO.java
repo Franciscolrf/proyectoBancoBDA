75e6d3f7e11318d0af9b95ad3275c2b1d845c70c
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-02-20 04:52:55 -0700

    Implementación
    Implementación de la interfaz ISinCuentaDAO

--- PersistenciaBanco/src/main/java/dao/ISinCuentaDAO.java
+++ PersistenciaBanco/src/main/java/dao/ISinCuentaDAO.java
@@ -4,10 +4,40 @@
  */
 package dao;
 
+import com.mycompany.dominiobanco.SinCuenta;
+
+import dto.SinCuentaNuevaDTO;
+import excepciones.PersistenciaException;
+
 /**
- *
+ * Esta interfaz posee los métodos para acceder y manipular los datos de la tabla SinCuenta en la base de datos.
  * @author ID145
  */
 public interface ISinCuentaDAO {
     
+    /**
+     * Este método se encarga de agregar una nueva transacción a la base de datos.
+     * 
+     * @param sinCuentaNueva DTO con los datos de la transacción a agregar
+     * @return Transacción agregada
+     * @throws PersistenciaException
+     */
+    SinCuenta agregar(SinCuentaNuevaDTO sinCuentaNueva) throws PersistenciaException;
+
+    /**
+     * Este método se encarga de consultar una transacción por su ID.
+     * @param id_transaccion 
+     * @return Transacción consultada
+     * @throws PersistenciaException
+     */
+    SinCuenta consultar(int id_transaccion) throws PersistenciaException;
+
+    /**
+     * Este método se encarga de consultar una transacción por su ID.
+     * @param folio
+     * @param contrasena
+     * @return Transacción consultada sin cuenta.
+     * @throws PersistenciaException
+     */
+    SinCuenta consultarSinCuenta(int folio, String contrasena) throws PersistenciaException;
 }
